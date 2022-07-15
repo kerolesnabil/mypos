7112fb39811a5700223502c40a85ea7831b6f128
keroles <keroles.nabil.official@gmail.com>
2022-07-15 22:36:54 +0200

    first project

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.dashboard.app')
+
+@section('content')
+
+    <div class="content-wrapper">
+        <section class="content-header">
+
+         
+
+
+        </section>
+
+        <section class="content">
+
+            <div class="box box-primary">
+
+                <div class="box-header">
+
+                </div><!-- end of box header -->
+                <div class="box-body">
+
+
+                </div><!-- end of box body -->
+
+            </div><!-- end of box -->
+
+        </section><!-- end of content -->
+
+    </div><!-- end of content wrapper -->
+
+@endsection
